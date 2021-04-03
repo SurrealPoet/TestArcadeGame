ae3d384fa2eecd1782e698cf67c43968a6815de3
kyle lacour <kdlacour@yahoo.com>
2021-04-03 01:11:02 -0700

    fixed misspelling of __init__ for Player class and made it where player can move

--- main.py
+++ main.py
@@ -25,36 +25,32 @@ BLACK = (0, 0, 0)
 
 
 class Player:
-    def __int__(self, x=450, y=450, color=WHITE):
+    def __init__(self, x=450, y=450, color=WHITE):
         self.x = x
         self.y = y
         self.color = color
-        #self.name = name
+        # self.name = name
 
-    def set_player(self, x, y, color):
-        pygame.draw.circle(DISPLAYSURF, color, (x, y), PLAYERSIZE)
+    # def set_player(self, x, y, color):
+    #     pygame.draw.circle(DISPLAYSURF, color, (x, y), PLAYERSIZE)
 
     def draw_player(self):
-        pygame.draw.circle(DISPLAYSURF, GRAY, (self.x, self.y), PLAYERSIZE)
+        pygame.draw.circle(DISPLAYSURF, self.color, (self.x, self.y), PLAYERSIZE)
 
     def set_location(self, x, y):
-        pass
-        # self.x += x
-        # self.y += y
-        # print(self.x, self.y)
-        # return self.x, self.y
+        self.x += x
+        self.y += y
+        print(self.x, self.y)
+        return self.x, self.y
 
     def get_location(self):
         pass
-        #return self.x, self.y
+        # return self.x, self.y
 
 
 def run_game():
     running = True
     while running:
-        p3 = Player(x=200, y=200, color=RED)
-        #p3.draw_player()
-        # p3.set_location(1, 1)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -75,6 +71,7 @@ def run_game():
 def main():
     global FPSCLOCK, DISPLAYSURF
     pygame.init()
+    pygame.key.set_repeat(100)
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     DISPLAYSURF.fill(BGCOLOR)
@@ -86,8 +83,30 @@ def main():
     # p2 = Player()
     # p2.set_player(200, 200, GREEN)
     # p1.get_location()
+    p3 = Player(x=200, y=200, color=RED)
+    p3.draw_player()
+    p3.set_location(1, 1)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                # print(event.key)
+                if event.key == pygame.K_w:
+                    p3.set_location(0, -10)
+                if event.key == pygame.K_s:
+                    p3.set_location(0, 10)
+                if event.key == pygame.K_d:
+                    p3.set_location(10, 0)
+                if event.key == pygame.K_a:
+                    p3.set_location(-10, 0)
+        DISPLAYSURF.fill(BGCOLOR)
+        p3.draw_player()
+        pygame.display.update()
 
-    run_game()
+    # run_game()
 
 
 main()
